4fd44cd483ea2c69409fa96e379b1642244a6813
Luigi <luigi@bitso.com>
2022-07-01 13:08:34 -0500

    First commit

---
+++ cypress/integration/search_assets.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe("Search for specific Assests", () => {
+  describe("when typing an asset name", () => {
+    it("should be filtered the ones that match with the searched word", () => {});
+  });
+
+  describe("when typing an invalid asset name", () => {
+    it("should be displayed an empty state", () => {});
+  });
+});
